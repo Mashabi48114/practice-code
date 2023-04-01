4fc273f0ab0720f4ea4819873161954d027259cf
Mashabi48114 <129622256+Mashabi48114@users.noreply.github.com>
2023-04-02 01:12:26 +0600

    Add files via upload

---
+++ task 5.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int sum(int x,int y);
+int main(){
+int x=5,y=5,p;
+p=sum(5,5);
+cout<<p;
+}
+int sum(int x,int y){
+
+int s;
+s=x*y;
+return s;
+
+
+}
+
+
+
+
